ac9b24253f0134ca5ed96268356e9d28330d9c83
Samuel Caldas <samuel.caldas@gmail.com>
2021-03-25 05:11:48 -0300

    Update PPO-Keras.cs

--- PPO-Keras.cs
+++ PPO-Keras.cs
@@ -62,23 +62,25 @@ var (DUMMY_ACTION, DUMMY_VALUE) = np.zeros((1, NUM_ACTIONS)), np.zeros((1, 1));
 
 
 
-} static object exponential_average(object old, object new, object b1){
-    return old * b1 + (1 - b1) * new;
-
-
-} static object proximal_policy_optimization_loss(object advantage, object old_prediction) {
-    static object loss(object y_true, object y_pred) {
-        prob = np.sum(y_true * y_pred, axis: -1);
-        old_prob = np.sum(y_true * old_prediction, axis: -1);
+static object exponential_average(double old, double _new, double b1)
+{
+    return old * b1 + (1 - b1) * _new;
+}
+static Tensor proximal_policy_optimization_loss(double advantage, double old_prediction)
+{
+    static Tensor loss(double y_true, double y_pred)
+    {
+        var prob = np.sum(y_true * y_pred, axis: -1);
+        var old_prob = np.sum(y_true * old_prediction, axis: -1);
         r = prob / (old_prob + 1e-10);
         return -np.mean(np.minimum(r * advantage, np.clip(r, min_value: 1 - LOSS_CLIPPING, max_value: 1 + LOSS_CLIPPING) * advantage) + ENTROPY_LOSS * -(prob * np.log(prob + 1e-10)));
     }
-    return loss;
-
-
-
-} static object proximal_policy_optimization_loss_continuous(object advantage, object old_prediction) {
-    static object loss(object y_true, object y_pred) {
+    return loss(advantage, old_prediction);
+}
+static Tensor proximal_policy_optimization_loss_continuous(object advantage, object old_prediction)
+{
+    static Tensor loss(object y_true, object y_pred)
+    {
         var = np.square(NOISE);
         pi = 3.1415926;
         denom = np.sqrt(2 * pi * var);
@@ -91,31 +93,34 @@ var (DUMMY_ACTION, DUMMY_VALUE) = np.zeros((1, NUM_ACTIONS)), np.zeros((1, 1));
 
         return -np.mean(np.minimum(r * advantage, np.clip(r, min_value: 1 - LOSS_CLIPPING, max_value: 1 + LOSS_CLIPPING) * advantage));
     }
-    return loss;
+    return loss(advantage, old_prediction);
 }
 
 
 class Agent {
-    static object Agent(){
-        this.critic = this.build_critic();
+    static object Agent()
+    {
+        var critic = build_critic();
         if (CONTINUOUS == false)
-            this.actor = this.build_actor();
+            var actor = build_actor();
         else
-            this.actor = this.build_actor_continuous();
+            var actor = build_actor_continuous();
 
-        this.env = new CartPoleEnv(WinFormEnvViewer.Factory);
-        Console.WriteLine(this.env.ActionSpace, "action_space", this.env.ObservationSpace, "observation_space");
-        this.episode = 0;
-        this.observation = this.env.Reset();
-        this.val = false;
-        this.reward = new List<double>();
-        this.reward_over_time = new List<double>();
-        this.name = this.get_name();
-        this.writer = SummaryWriter(this.name);
-        this.gradient_steps = 0;
+        var env = new CartPoleEnv(WinFormEnvViewer.Factory);
+        Console.WriteLine(env.ActionSpace, "action_space", env.ObservationSpace, "observation_space");
+        var episode = 0;
+        var observation = env.Reset();
+        var val = false;
+        var reward = new List<double>();
+        var reward_over_time = new List<double>();
+        var name = get_name();
+        var writer = SummaryWriter(name);
+        var gradient_steps = 0;
 
-    } static object get_name(){
-        name = "AllRuns/";
+    }
+    static object get_name()
+    {
+        var name = "AllRuns/";
         if (CONTINUOUS == true)
             name += "continous/";
         else
@@ -123,141 +128,149 @@ class Agent {
         name += ENV;
         return name;
 
-    } static object build_actor(){
-        state_input = Input(shape: (NUM_STATE,));
-        advantage = Input(shape: (1,));
-        old_prediction = Input(shape: (NUM_ACTIONS,));
+    }
+    static object build_actor()
+    {
+        var state_input = Input(shape: (NUM_STATE,));
+        var advantage = Input(shape: (1,));
+        var old_prediction = Input(shape: (NUM_ACTIONS,));
 
-        x = Dense(HIDDEN_SIZE, activation: "tanh").Apply(state_input);
-        for (int _ in range(NUM_LAYERS - 1))
+        var x = Dense(HIDDEN_SIZE, activation: "tanh").Apply(state_input);
+        for (int _ <= NUM_LAYERS - 1))
             x = Dense(HIDDEN_SIZE, activation: "tanh").Apply(x);
 
-        out_actions = Dense(NUM_ACTIONS, activation: "softmax", name: "output").Apply(x);
+        var out_actions = Dense(NUM_ACTIONS, activation: "softmax", name: "output").Apply(x);
 
-        model = Model(inputs:[state_input, advantage, old_prediction], outputs:[out_actions]);
-        model.compile(optimizer: Adam(lr: LR), loss:[proximal_policy_optimization_loss(advantage: advantage, old_prediction: old_prediction)]);
+        model = Model(inputs: new[] { state_input, advantage, old_prediction }, outputs: new[] { out_actions });
+        model.compile(optimizer: Adam(lr: LR), loss: new[] { proximal_policy_optimization_loss(advantage: advantage, old_prediction: old_prediction) });
         model.summary();
 
         return model;
 
-    } static object build_actor_continuous(){
-        state_input = Input(shape: (NUM_STATE,));
-        advantage = Input(shape: (1,));
-        old_prediction = Input(shape: (NUM_ACTIONS,));
+    }
+    static object build_actor_continuous()
+    {
+        var state_input = Input(shape: (NUM_STATE,));
+        var advantage = Input(shape: (1,));
+        var old_prediction = Input(shape: (NUM_ACTIONS,));
 
-        x = Dense(HIDDEN_SIZE, activation: "tanh").Apply(state_input);
-        for (int _ in range(NUM_LAYERS - 1))
+        var x = Dense(HIDDEN_SIZE, activation: "tanh").Apply(state_input);
+        for (int _ <= NUM_LAYERS - 1))
             x = Dense(HIDDEN_SIZE, activation: "tanh").Apply(x);
 
-        out_actions = Dense(NUM_ACTIONS, name: "output", activation: "tanh").Apply(x);
+        var out_actions = Dense(NUM_ACTIONS, name: "output", activation: "tanh").Apply(x);
 
-        model = Model(inputs:[state_input, advantage, old_prediction], outputs:[out_actions]);
-        model.compile(optimizer: Adam(lr: LR), loss:[proximal_policy_optimization_loss_continuous(advantage: advantage, old_prediction: old_prediction)]);
+        var model = new Model(inputs: new[] { state_input, advantage, old_prediction }, outputs: new[] {out_actions});
+        model.compile(optimizer: Adam(lr: LR), loss:new[] { proximal_policy_optimization_loss_continuous(advantage: advantage, old_prediction: old_prediction) });
         model.summary();
 
         return model;
+    }
+    static object build_critic()
+    {
 
-    } static object build_critic(){
-
-        state_input = Input(shape: (NUM_STATE,));
-        x = Dense(HIDDEN_SIZE, activation: "tanh").Apply(state_input);
-        for (int _ in range(NUM_LAYERS - 1))
+        var state_input = Input(shape: (NUM_STATE,));
+        var x = Dense(HIDDEN_SIZE, activation: "tanh").Apply(state_input);
+        for (int _ <= NUM_LAYERS - 1))
             x = Dense(HIDDEN_SIZE, activation: "tanh").Apply(x);
 
-        out_value = Dense(1).Apply(x);
+        var out_value = Dense(1).Apply(x);
 
-        model = Model(inputs:[state_input], outputs:[out_value]);
+        var model = Model(inputs: new[] { state_input }, outputs: new[] { out_value });
         model.compile(optimizer: Adam(lr: LR), loss: "mse");
 
         return model;
-
-    } static object reset_env(){
-        this.episode += 1;
-        if (this.episode % 100 == 0)
-            this.val = true;
+    }
+    static object reset_env()
+    {
+        episode += 1;
+        if (episode % 100 == 0)
+            var val = true;
         else
-            this.val = false;
-        this.observation = this.env.Reset();
-        this.reward = new List<double>();
-
-    } static object get_action(){
-        p = this.actor.predict([this.observation.reshape(1, NUM_STATE), DUMMY_VALUE, DUMMY_ACTION]);
-        if (this.val == false)
+            var val = false;
+        var observation = env.Reset();
+        var reward = new List<double>();
+    }
+    static object get_action()
+    {
+        var p = actor.predict(new[] { observation.reshape(1, NUM_STATE), DUMMY_VALUE, DUMMY_ACTION });
+        if (val == false)
             action = np.random.choice(NUM_ACTIONS, probabilities: np.nan_to_num(p[0]));
         else
             action = np.argmax(p[0]);
         action_matrix = np.zeros(NUM_ACTIONS);
         action_matrix[action] = 1;
-        return action, action_matrix, p;
-
-    } static object get_action_continuous(){
-        p = this.actor.predict([this.observation.reshape(1, NUM_STATE), DUMMY_VALUE, DUMMY_ACTION]);
-        if (this.val == false)
+        return (action, action_matrix, p);
+    }
+    static object get_action_continuous()
+    {
+        p = actor.predict([observation.reshape(1, NUM_STATE), DUMMY_VALUE, DUMMY_ACTION]);
+        if (val == false)
             action = action_matrix = p[0] + np.random.normal(loc: 0, scale: NOISE, size: p[0].shape);
         else
             action = action_matrix = p[0];
         return (action, action_matrix, p);
-
-    } static object transform_reward(){
-        if (this.val == true)
-            this.writer.add_scalar("Val episode reward", np.array(this.reward).sum(), this.episode);
+    }
+    static object transform_reward()
+    {
+        if (val == true)
+            writer.add_scalar("Val episode reward", np.array(reward).sum(), episode);
         else
-            this.writer.add_scalar("Episode reward", np.array(this.reward).sum(), this.episode);
-        for (int j in range(len(this.reward) - 2, -1, -1))
-            this.reward[j] += this.reward[j + 1] * GAMMA;
-
-    } static object get_batch(){
-        batch = [[], [], [], []];
+            writer.add_scalar("Episode reward", np.array(reward).sum(), episode);
+        for (int j <= len(reward) - 2, -1, -1))
+            reward[j] += reward[j + 1] * GAMMA;
+    }
+    static object get_batch(){
+        var batch = [[], [], [], []];
 
         tmp_batch = [[], [], []];
         while (len(batch[0]) < BUFFER_SIZE){
             if (CONTINUOUS==false)
-                action, action_matrix, predicted_action = this.get_action();
+                action, action_matrix, predicted_action = get_action();
             else
-                action, action_matrix, predicted_action = this.get_action_continuous();
-            var (observation, reward, done, info) = this.env.Step(action);
-            this.reward.Add(reward);
+                action, action_matrix, predicted_action = get_action_continuous();
+            var (observation, reward, done, info) = env.Step(action);
+            reward.Add(reward);
 
-            tmp_batch[0].Add(this.observation);
+            tmp_batch[0].Add(observation);
             tmp_batch[1].Add(action_matrix);
             tmp_batch[2].Add(predicted_action);
-            this.observation = observation;
+            observation = observation;
 
             if (done)
-                this.transform_reward();
-                if (this.val==false)
-                    for (int i in range(len(tmp_batch[0]))){
+                transform_reward();
+                if (val==false)
+                    for (int i <= len(tmp_batch[0]))){
                         var (obs, action, pred) = (tmp_batch[0][i], tmp_batch[1][i], tmp_batch[2][i]);
-                        r = this.reward[i];
+                        r = reward[i];
                         batch[0].Add(obs);
                         batch[1].Add(action);
                         batch[2].Add(pred);
                         batch[3].Add(r);
                     }
                 }
-                tmp_batch = [[], [], []];
-                this.reset_env();
+                var tmp_batch = [[], [], []];
+                reset_env();
             }
         }
         var (obs, action, pred, reward) = (np.array(batch[0]), np.array(batch[1]), np.array(batch[2]), np.reshape(np.array(batch[3]), (len(batch[3]), 1)));
         pred = np.reshape(pred, (pred.shape[0], pred.shape[2]));
         return (obs, action, pred, reward);
-
     } static object run(){
-        while (this.episode < EPISODES){
-            obs, action, pred, reward = this.get_batch();
-            obs, action, pred, reward = obs[:BUFFER_SIZE], action[:BUFFER_SIZE], pred[:BUFFER_SIZE], reward[:BUFFER_SIZE];
+        while (episode < EPISODES){
+            obs, action, pred, reward = get_batch();
+            obs, action, pred, reward = obs[":BUFFER_SIZE"], action[":BUFFER_SIZE"], pred[":BUFFER_SIZE"], reward[":BUFFER_SIZE"];
             old_prediction = pred;
-            pred_values = this.critic.predict(obs);
+            pred_values = critic.predict(obs);
 
             advantage = reward - pred_values;
 
-            actor_loss = this.actor.fit([obs, advantage, old_prediction], [action], batch_size:BATCH_SIZE, shuffle:true, epochs:EPOCHS, verbose:false);
-            critic_loss = this.critic.fit([obs], [reward], batch_size:BATCH_SIZE, shuffle:true, epochs:EPOCHS, verbose:false);
-            this.writer.add_scalar("Actor loss", actor_loss.history["loss"][-1], this.gradient_steps);
-            this.writer.add_scalar("Critic loss", critic_loss.history["loss"][-1], this.gradient_steps);
+            actor_loss = actor.fit([obs, advantage, old_prediction], [action], batch_size:BATCH_SIZE, shuffle:true, epochs:EPOCHS, verbose:false);
+            critic_loss = critic.fit(new[] { obs }, new[] { reward }, batch_size:BATCH_SIZE, shuffle:true, epochs:EPOCHS, verbose:false);
+            writer.add_scalar("Actor loss", actor_loss.history["loss"][-1], gradient_steps);
+            writer.add_scalar("Critic loss", critic_loss.history["loss"][-1], gradient_steps);
 
-            this.gradient_steps += 1;
+            gradient_steps += 1;
         }
     }
 }
